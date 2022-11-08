6bfd9c5808c8847fb1555d52390d858d5c7270a1
Adam Fehnel <adam_fehnel@putnam.com>
2022-11-08 10:12:25 -0500

    It's Bootstrap!

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,9 @@
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
-
+	css: ['~/assets/styles/main.scss'],
+	script: [
+	  {
+		src: 'bootstrap/dist/js/bootstrap.bundle.min.js'
+	  }
+	],
 })
